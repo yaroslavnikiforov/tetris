5a7f1719ea5a29e206db41b3902f6fe20d8c0beb
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2019-09-15 23:18:09 +0300

    Move figures left and right

--- script.js
+++ script.js
@@ -139,3 +139,73 @@ function move() {
 }
 
 let interval = setInterval(move, 300);
+
+let flag = true;
+
+window.addEventListener("keydown", event => {
+  let coordinates1 = [
+    figureBody[0].getAttribute("posX"),
+    figureBody[0].getAttribute("posY")
+  ];
+  let coordinates2 = [
+    figureBody[1].getAttribute("posX"),
+    figureBody[1].getAttribute("posY")
+  ];
+  let coordinates3 = [
+    figureBody[2].getAttribute("posX"),
+    figureBody[2].getAttribute("posY")
+  ];
+  let coordinates4 = [
+    figureBody[3].getAttribute("posX"),
+    figureBody[3].getAttribute("posY")
+  ];
+
+  function getNewState(a) {
+    flag = true;
+
+    let figureNew = [
+      document.querySelector(
+        `[posX = "${+coordinates1[0] + a}"][posY = "${+coordinates1[1]}"]`
+      ),
+      document.querySelector(
+        `[posX = "${+coordinates2[0] + a}"][posY = "${+coordinates2[1]}"]`
+      ),
+      document.querySelector(
+        `[posX = "${+coordinates3[0] + a}"][posY = "${+coordinates3[1]}"]`
+      ),
+      document.querySelector(
+        `[posX = "${+coordinates4[0] + a}"][posY = "${+coordinates4[1]}"]`
+      )
+    ];
+
+    for (let i = 0; i < figureNew.length; i++) {
+      if (!figureNew[i] || figureNew[i].classList.contains("set")) {
+        flag = false;
+      }
+    }
+
+    if (flag) {
+      for (let i = 0; i < figureBody.length; i++) {
+        figureBody[i].classList.remove("figure");
+      }
+
+      figureBody = figureNew;
+
+      for (let i = 0; i < figureBody.length; i++) {
+        figureBody[i].classList.add("figure");
+      }
+    }
+  }
+
+  if (event.key === "ArrowLeft") {
+    getNewState(-1);
+  }
+
+  if (event.key === "ArrowRight") {
+    getNewState(1);
+  }
+
+  if (event.key === "ArrowDown") {
+    move();
+  }
+});
